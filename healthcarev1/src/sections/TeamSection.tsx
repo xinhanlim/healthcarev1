b3d1d476abdefbdef20eacc6a0f1090bad64c052
xinhanlim <xhxhan00@gmail.com>
2025-10-29 10:20:33 +0000

    changed font size for card

--- healthcarev1/src/sections/TeamSection.tsx
+++ healthcarev1/src/sections/TeamSection.tsx
@@ -26,58 +26,58 @@ export default function TeamSection() {
                 <div className="flex flex-col gap-y-4">
                     <img src="members/1.jpg" alt="member" className="border-3 border-[#282828]/70" />
                     <div className="pb-4 uppercase">
-                        <h1 className="text-h2 leading-none tracking-tighter">Jennis</h1>
-                        <h1 className="text-small pt-2 leading-none tracking-tighter section-header">Founder of Mech Tech </h1>
+                        <h1 className="sm:text-h2 text-h1 leading-none tracking-tighter ">Jennis</h1>
+                        <h1 className="text-base sm:text-small pt-2 leading-none tracking-tighter section-header">Founder of Mech Tech </h1>
                     </div>
                 </div>
                 <div className="flex flex-col gap-y-4">
                     <img src="members/2.jpg" alt="member" className="border-3 border-[#282828]/70" />
                     <div className="pb-4 uppercase">
-                        <h1 className="text-h2 leading-none tracking-tighter">DR Jennifer</h1>
-                        <h1 className="text-small pt-2 leading-none tracking-tighter section-header">Doctor</h1>
+                        <h1 className="sm:text-h2 text-h1 leading-none tracking-tighter">DR Jennifer</h1>
+                        <h1 className="text-base sm:text-small pt-2 leading-none tracking-tighter section-header">Doctor</h1>
                     </div>
                 </div>
                 <div className="flex flex-col gap-y-4">
                     <img src="members/4.jpg" alt="member" className="border-3 border-[#282828]/70" />
                     <div className="pb-4 uppercase">
-                        <h1 className="text-h2 leading-none tracking-tighter">DR Jasper</h1>
-                        <h1 className="text-small pt-2 leading-none tracking-tighter section-header">Doctor</h1>
+                        <h1 className="sm:text-h2 text-h1 leading-none tracking-tighter">DR Jasper</h1>
+                        <h1 className="text-base sm:text-small pt-2 leading-none tracking-tighter section-header">Doctor</h1>
                     </div>
                 </div>
                 <div className="flex flex-col gap-y-4">
                     <img src="members/3.jpg" alt="member" className="border-3 border-[#282828]/70" />
                     <div className="pb-4 uppercase">
-                        <h1 className="text-h2 leading-none tracking-tighter">Jasmine</h1>
-                        <h1 className="text-small pt-2 leading-none tracking-tighter section-header">Design Team Lead</h1>
+                        <h1 className="sm:text-h2 text-h1 leading-none tracking-tighter">Jasmine</h1>
+                        <h1 className="text-base sm:text-small pt-2 leading-none tracking-tighter section-header">Design Team Lead</h1>
                     </div>
                 </div>
 
                 <div className="flex flex-col gap-y-4">
                     <img src="members/8.jpg" alt="member" className="border-3 border-[#282828]/70" />
                     <div className="pb-4 uppercase">
-                        <h1 className="text-h2 leading-none tracking-tighter">Melody</h1>
-                        <h1 className="text-small pt-2 leading-none tracking-tighter section-header">UX/UI DESIGNER</h1>
+                        <h1 className="sm:text-h2 text-h1 leading-none tracking-tighter">Melody</h1>
+                        <h1 className="text-base sm:text-small pt-2 leading-none tracking-tighter section-header">UX/UI DESIGNER</h1>
                     </div>
                 </div>
                 <div className="flex flex-col gap-y-4">
                     <img src="members/5.jpg" alt="member" className="border-3 border-[#282828]/70" />
                     <div className="pb-4 uppercase">
-                        <h1 className="text-h2 leading-none tracking-tighter">Justin</h1>
-                        <h1 className="text-small pt-2 leading-none tracking-tighter section-header">Software Team Lead</h1>
+                        <h1 className="sm:text-h2 text-h1 leading-none tracking-tighter">Justin</h1>
+                        <h1 className="text-base sm:text-small pt-2 leading-none tracking-tighter section-header">Software Team Lead</h1>
                     </div>
                 </div>
                 <div className="flex flex-col gap-y-4">
                     <img src="members/6.jpg" alt="member" className="border-3 border-[#282828]/70" />
                     <div className="pb-4 uppercase">
-                        <h1 className="text-h2 leading-none tracking-tighter">James</h1>
-                        <h1 className="text-small pt-2 leading-none tracking-tighter section-header">Software Developer</h1>
+                        <h1 className="sm:text-h2 text-h1 leading-none tracking-tighter">James</h1>
+                        <h1 className="text-base sm:text-small pt-2 leading-none tracking-tighter section-header">Software Developer</h1>
                     </div>
                 </div>
                 <div className="flex flex-col gap-y-4">
                     <img src="members/7.jpg" alt="member" className="border-3 border-[#282828]/70" />
                     <div className="pb-4 uppercase">
-                        <h1 className="text-h2 leading-none tracking-tighter">Bond</h1>
-                        <h1 className="text-small pt-2 leading-none tracking-tighter section-header">Software Developer</h1>
+                        <h1 className="sm:text-h2 text-h1 leading-none tracking-tighter">Bond</h1>
+                        <h1 className="text-base sm:text-small pt-2 leading-none tracking-tighter section-header">Software Developer</h1>
                     </div>
                 </div>
 
