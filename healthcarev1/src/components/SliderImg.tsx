f44304918382231edb32a01ab97dd71dd1cfc163
xinhanlim <xhxhan00@gmail.com>
2025-10-30 10:31:03 +0000

    still thinking aboutt he reverse scroll

--- healthcarev1/src/components/SliderImg.tsx
+++ healthcarev1/src/components/SliderImg.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { motion } from 'framer-motion';
+import { motion} from 'framer-motion';
 
 export default function SliderImg() {
     const images = [
@@ -12,28 +12,34 @@ export default function SliderImg() {
     ]
     const duplicatedImages = [...images, ...images]
 
+
+
     return (
         <div className="flex overflow-hidden justify py-14 bg-[#fefefe]">
-            {duplicatedImages.map((item, index) => (
-                <motion.div
-                    key={index}
-                    className="flex-shrink-0 h-10 mx-4"
-                    animate={{
-                        x: [0, -100  * images.length]
-                    }}
-                    transition={{
-                        duration: 10,
-                        repeat: Infinity,
-                        ease: "linear",
-                    }}
-                >
-                    <img
-                        src={item.img}
-                        alt={`Slide ${index}`}
-                        className="w-full h-full object-contain" // Adjust size as needed
-                    />
-                </motion.div>
-            ))}
+             <motion.div 
+                className="flex"
+                animate={{
+                    x:  [0, -100 * images.length]  // Right to left
+                }}
+                transition={{
+                    duration: 10,
+                    repeat: Infinity,
+                    ease: "linear",
+                }}
+            >
+                {duplicatedImages.map((item, index) => (
+                    <div
+                        key={index}
+                        className="flex-shrink-0 h-10 mx-4"
+                    >
+                        <img
+                            src={item.img}
+                            alt={`Slide ${index}`}
+                            className="w-full h-full object-contain"
+                        />
+                    </div>
+                ))}
+            </motion.div>
         </div>
     )
 }
