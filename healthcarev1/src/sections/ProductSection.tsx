643734d494e2482b77c9fd8c23bca3e4a6448774
xinhanlim <xhxhan00@gmail.com>
2025-10-30 10:05:05 +0000

    added pb-20 instead of pb-10 prodcut section

--- healthcarev1/src/sections/ProductSection.tsx
+++ healthcarev1/src/sections/ProductSection.tsx
@@ -12,7 +12,7 @@ return(
                 transition={{ duration: 0.8 }}
                 className="border-b border-[#282828]/50 z-30">
             <h1 className="z-30 text-h1 leading-none tracking-tighter section-header ">//Product</h1>
-            <h1 className="z-30 text-h4 pt-4 pb-10 sm:text-h6 sm:max-w-[50%] leading-none tracking-tighter  ">Supporting Patients, Enabling Physicians</h1>
+            <h1 className="z-30 text-h4 pt-4 pb-20 sm:text-h6 sm:max-w-[50%] leading-none tracking-tighter  ">Supporting Patients, Enabling Physicians</h1>
         </motion.div>
         <div className="flex flex-col">
             <Products 
