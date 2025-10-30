f6ef6135e5daa0fcb8936e577ec48fb4005705a8
xinhanlim <xhxhan00@gmail.com>
2025-10-30 11:05:58 +0000

    able to parallax scroll

--- healthcarev1/src/sections/NewSection.tsx
+++ healthcarev1/src/sections/NewSection.tsx
@@ -5,8 +5,8 @@ import {motion} from 'framer-motion'
 
 export default function NewSection(){
     return (
-    <section id="news">
-        <div className="transform -rotate-2 relative pt-20">
+    <section id="news" className="relative z-50 bg-[#fefefe]">
+        <div className="">
             <SliderImg/>
         </div>
 
@@ -15,8 +15,8 @@ export default function NewSection(){
                 whileInView={{ opacity: 1, y: 0 }}
                 viewport={{ once: true}}
                 transition={{ duration: 0.8 }}
-                className="h-svh w-full flex flex-col sm:pb-20 sm:px-[5%] px-6 pt-10 sm:pt-24">
-            <h1 className="z-20 text-h1 leading-none tracking-tighter section-header ">//News</h1>
+                className="h-svh w-full flex flex-col sm:pb-20 sm:px-[5%] px-6 sm:pt-24">
+            <h1 className="text-h1 leading-none tracking-tighter section-header ">//News</h1>
         </motion.div>
         <div className="grid grid-cols-1 sm:grid-cols-2 sm:pb-20 sm:px-[5%] px-6">  
             <NewsCard/>
--- healthcarev1/src/sections/ProductSection.tsx
+++ healthcarev1/src/sections/ProductSection.tsx
@@ -4,17 +4,17 @@ import {motion } from 'framer-motion';
 
 export default function ProductionSection(){
 return(
-    <section id="product" className="relative h-full w-full flex flex-col sm:pb-20 sm:px-[5%] px-6 pt-10 sm:pt-24 ">
+    <section id="product" className="sticky top-0 z-20 h-full w-full flex flex-col sm:px-[5%] px-6 sm:pt-12 ">
         <motion.div
                 initial={{ opacity: 0, y: 50 }}
                 whileInView={{ opacity: 1, y: 0 }}
                 viewport={{ once: true}}
                 transition={{ duration: 0.8 }}
-                className="border-b border-[#282828]/50 z-30">
-            <h1 className="z-30 text-h1 leading-none tracking-tighter section-header ">//Product</h1>
-            <h1 className="z-30 text-h4 pt-4 pb-20 sm:text-h6 sm:max-w-[50%] leading-none tracking-tighter  ">Supporting Patients, Enabling Physicians</h1>
+                className="border-b border-[#282828]/50">
+            <h1 className=" text-h1 leading-none tracking-tighter section-header ">//Product</h1>
+            <h1 className="text-h4 pt-4 pb-20 sm:text-h6 sm:max-w-[50%] leading-none tracking-tighter  ">Supporting Patients, Enabling Physicians</h1>
         </motion.div>
-        <div className="flex flex-col">
+        <div className="flex flex-col pb-40">
             <Products 
             title = "AuraSense Smart Inhaler"/>
             <Products 
