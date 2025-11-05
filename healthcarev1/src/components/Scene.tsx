94719188e0295e1353fc279f52437f8bf4fff8d7
xinhanlim <xhxhan00@gmail.com>
2025-11-05 04:29:24 +0000

    added w-full h-full testing

--- healthcarev1/src/components/Scene.tsx
+++ healthcarev1/src/components/Scene.tsx
@@ -23,8 +23,8 @@ export default function Scene() {
     return (
         
         
-        <div className="absolute inset-0">
-            <Canvas gl={{ alpha: true, antialias: true }} className="h-svh" style={{ background: "transparent" }}>
+        <div className="absolute inset-0 h-svh w-full">
+            <Canvas gl={{ alpha: true, antialias: true }}  style={{ background: "transparent" }}>
                 <ambientLight intensity={0.8} />
                 <directionalLight position={[0, 4, 5]} intensity={1.2} color="#ffffff" />
                 <directionalLight position={[0, -2, 1]} intensity={0.5} color="#f0f0f0" />
